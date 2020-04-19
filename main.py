9208abac6c4db62034ff46931e2c281c12d9f61c
Ziyuan Ma <ziyuanma000@gmail.com>
2020-04-19 00:30:18 -0700

    change dataset

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import torch
+from model import Network
+import torchvision
+
 
 class model:
-    
+    def __init__(self):
+        self.network = Network()
+        self.data = 
+
+if __name__ == '__main__':
+    img = torchvision.datasets.ImageFolder('./STARE/')
+    print(img)
