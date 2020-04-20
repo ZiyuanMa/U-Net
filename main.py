2d04d65cf2c4dc032a7aeb77bc8b66a49a7a282b
Ziyuan Ma <ziyuanma000@gmail.com>
2020-04-20 00:25:26 -0700

    fix bugs

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 # random image transformation to do image augmentation
 trans = transforms.Compose([
     transforms.ToPILImage(),
-    transforms.RandomRotation(90), 
+    # transforms.RandomRotation(90), 
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.ToTensor(),
+    # transforms.Lambda(lambda x: x/255)
 ])
 
 class UNetDataset(Dataset):
@@ -35,11 +36,24 @@ class UNetDataset(Dataset):
             img = Image.open(img_path)
             img = transforms.functional.center_crop(img, 512)
             img = transforms.functional.to_tensor(img)
-            img = img.unfold(1, 64, 32).unfold(2, 64, 32).permute(1,2,0,3,4).contiguous().view(225, 3, 64, 64)
+            img = img.unfold(1, 96, 32).unfold(2, 96, 32).permute(1,2,0,3,4).contiguous().view(196, 3, 96, 96)
             for i, sub_img in enumerate(img):
                 # discard corner image 
                 if i not in [0, 1, 13, 14, 15, 29, 195, 209, 210, 211, 223, 224]:
                     self.imgs.append(sub_img)
+        
+        self.masks_path = list(sorted(os.listdir(os.path.join(root, "mask"))))
+        self.masks = []
+        for path in self.masks_path:
+            mask_path = os.path.join(self.root, "mask", path)
+            mask = Image.open(mask_path)
+            mask = transforms.functional.center_crop(mask, 512)
+            mask = transforms.functional.to_tensor(mask)
+            mask = mask.unfold(1, 96, 32).unfold(2, 96, 32).permute(1,2,0,3,4).contiguous().view(196, 1, 96, 96)
+            for i, sub_mask in enumerate(mask):
+                # discard corner image 
+                if i not in [0, 1, 13, 14, 15, 29, 195, 209, 210, 211, 223, 224]:
+                    self.masks.append(sub_mask)
 
         self.targets_path = list(sorted(os.listdir(os.path.join(root, "1st_manual"))))
         self.targets = []
@@ -48,7 +62,7 @@ class UNetDataset(Dataset):
             target = Image.open(target_path)
             target = transforms.functional.center_crop(target, 512)
             target = transforms.functional.to_tensor(target)
-            target = target.unfold(1, 64, 32).unfold(2, 64, 32).permute(1,2,0,3,4).contiguous().view(225, 1, 64, 64)
+            target = target.unfold(1, 96, 32).unfold(2, 96, 32).permute(1,2,0,3,4).contiguous().view(196, 1, 96, 96)
             for i, sub_target in enumerate(target):
                 if i not in [0, 1, 13, 14, 15, 29, 195, 209, 210, 211, 223, 224]:
                     self.targets.append(sub_target)
@@ -63,11 +77,26 @@ class UNetDataset(Dataset):
         random.seed(seed)
         img = self.transforms(img)
 
+
+        mask = self.masks[idx]
+        random.seed(seed)
+        mask = self.transforms(mask)
+
         target = self.targets[idx]
         random.seed(seed)
         target = self.transforms(target)
 
-        return img.to(device), target.to(device)
+        # fig = plt.figure()
+        # ax1 = fig.add_subplot(1,3,1)
+        # ax1.imshow(img.permute((1,2,0)))
+        # ax2 = fig.add_subplot(1,3,2)
+        # ax2.imshow(torch.squeeze(mask), cmap="gray")
+        # ax3 = fig.add_subplot(1,3,3)
+        # ax3.imshow(torch.squeeze(target), cmap="gray")
+        # plt.show()
+
+
+        return img.to(device), mask.to(device), target.to(device)
 
     def __len__(self):
 
@@ -79,32 +108,42 @@ class model:
         self.data_path = data_path
         
         self.network = Network()
-        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=1e-4)
+        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=1e-3)
         self.loss_fn = nn.BCELoss()
 
         # training iteration
-        self.epoch = 50
+        self.epoch = 100
 
     def train(self):
         self.network.to(device)
+        self.network.train()
         training_set = UNetDataset(os.path.join(self.data_path, 'training'), trans)
-        training_loader = DataLoader(training_set, batch_size=24, shuffle=True)
+        training_loader = DataLoader(training_set, batch_size=16, shuffle=True)
         for i in range(self.epoch):
             print('{} epoch {} {}'.format('=' * 10, i, '=' * 10))
             sum_loss = 0
             
-            for img, target in tqdm(training_loader):
+            for img, mask, target in tqdm(training_loader):
+
                 predict = self.network(img)
-                
+
+                predict = predict.view(predict.size(0), -1)
+                predict = predict * mask.view(mask.size(0), -1)
+                target = target.view(target.size(0), -1)
+
                 self.optimizer.zero_grad()
                 loss = self.loss_fn(predict, target)
-                loss.backward()
                 sum_loss += loss.item()
+                loss.backward()
+                self.optimizer.step()
 
             sum_loss /= 178
             print('loss: {}'.format(sum_loss))
 
-            torch.save(self.network.state_dict(), './model{}.pth'.format(i))
+            if i % 10 == 0:
+                torch.save(self.network.state_dict(), './model{}.pth'.format(i))
+
+        torch.save(self.network.state_dict(), './model.pth')
 
     # def predict(self):
     #     self.network.load_state_dict(torch.load('./model.pth'))
@@ -120,7 +159,7 @@ class model:
         '''
         # load saved model
         self.network.to('cpu')
-        self.network.load_state_dict(torch.load('./model.pth', map_location=torch.device('cpu')))
+        self.network.load_state_dict(torch.load('./model95.pth', map_location=torch.device('cpu')))
         self.network.eval()
 
         # load test set
@@ -154,8 +193,14 @@ class model:
                 # print(AC)
 
                 # show predicted image
+                fig = plt.figure()
+                ax1 = fig.add_subplot(1,2,1)
+                ax1.imshow(predict, cmap="gray")
+                ax2 = fig.add_subplot(1,2,2)
+                ax2.imshow(target, cmap="gray")
                 # plt.imshow(predict, cmap="gray")
-                # plt.show()
+                # plt.imshow(target, cmap="gray")
+                plt.show()
 
         print('accuracy: %.4f' %(sum(ac_list)/20))
 
@@ -164,4 +209,4 @@ if __name__ == '__main__':
 
     m = model('./DRIVE')
     m.train()
-    m.test()
+    # m.test()
