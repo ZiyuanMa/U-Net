81c91c0ed9825bd41c402033f78d4df9f290051c
Ziyuan Ma <ziyuanma000@gmail.com>
2020-04-21 01:42:39 -0700

    complete function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
-from model import Network
+import torch.optim as optim
+from model import R2UNet, UNet, IterNet
 import torchvision
 from PIL import Image
 import os
@@ -10,12 +11,22 @@ from tqdm import tqdm
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, datasets
 import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+import argparse
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
+
+trans_fn1 = transforms.Compose([
+    transforms.CenterCrop(512),
+    transforms.RandomRotation(180),
+    transforms.ToTensor(),
+])
+
 # random image transformation to do image augmentation
-trans = transforms.Compose([
+trans_fn2 = transforms.Compose([
     transforms.ToPILImage(),
-    # transforms.RandomRotation(90), 
+    # transforms.RandomRotation(90),
+    transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.ToTensor(),
@@ -26,33 +37,32 @@ class UNetDataset(Dataset):
     def __init__(self, root, transform=None):
         self.root = root
         self.transforms = transform
-
-        # cut all images to 64x64 due to GPU memory limit 
-
-        self.imgs_path = list(sorted(os.listdir(os.path.join(root, "images"))))
+        seed = random.randint(0, 2**32)
+        self.imgs_path = list(sorted(os.listdir('./DRIVE/training/images')))
         self.imgs = []
         for path in self.imgs_path:
-            img_path = os.path.join(self.root, "images", path)
+            img_path = os.path.join('./DRIVE/training/images', path)
             img = Image.open(img_path)
-            img = transforms.functional.center_crop(img, 512)
-            img = transforms.functional.to_tensor(img)
+            # image augmentation
+            random.seed(seed) 
+            img = trans_fn1(img)
+            # cut the 512x512 image to 96x96 with stride 32 which is 196 sub images
             img = img.unfold(1, 96, 32).unfold(2, 96, 32).permute(1,2,0,3,4).contiguous().view(196, 3, 96, 96)
             for i, sub_img in enumerate(img):
-                # discard corner image 
-                if i not in [0, 1, 13, 14, 15, 29, 195, 209, 210, 211, 223, 224]:
+                # add all sub images but not corner sub images 
+                if i not in [0, 1, 14, 12, 13, 27, 168, 182, 183, 181, 194, 195]:
                     self.imgs.append(sub_img)
         
-        self.masks_path = list(sorted(os.listdir(os.path.join(root, "mask"))))
+        self.masks_path = list(sorted(os.listdir(os.path.join('./DRIVE/training/mask'))))
         self.masks = []
         for path in self.masks_path:
             mask_path = os.path.join(self.root, "mask", path)
             mask = Image.open(mask_path)
-            mask = transforms.functional.center_crop(mask, 512)
-            mask = transforms.functional.to_tensor(mask)
+            random.seed(seed)
+            mask = trans_fn1(mask)
             mask = mask.unfold(1, 96, 32).unfold(2, 96, 32).permute(1,2,0,3,4).contiguous().view(196, 1, 96, 96)
             for i, sub_mask in enumerate(mask):
-                # discard corner image 
-                if i not in [0, 1, 13, 14, 15, 29, 195, 209, 210, 211, 223, 224]:
+                if i not in [0, 1, 14, 12, 13, 27, 168, 182, 183, 181, 194, 195]:
                     self.masks.append(sub_mask)
 
         self.targets_path = list(sorted(os.listdir(os.path.join(root, "1st_manual"))))
@@ -60,31 +70,31 @@ class UNetDataset(Dataset):
         for path in self.targets_path:
             target_path = os.path.join(self.root, "1st_manual", path)
             target = Image.open(target_path)
-            target = transforms.functional.center_crop(target, 512)
-            target = transforms.functional.to_tensor(target)
+            random.seed(seed)
+            target = trans_fn1(target)
             target = target.unfold(1, 96, 32).unfold(2, 96, 32).permute(1,2,0,3,4).contiguous().view(196, 1, 96, 96)
             for i, sub_target in enumerate(target):
-                if i not in [0, 1, 13, 14, 15, 29, 195, 209, 210, 211, 223, 224]:
+                if i not in [0, 1, 14, 12, 13, 27, 168, 182, 183, 181, 194, 195]:
                     self.targets.append(sub_target)
 
 
     def __getitem__(self, idx):
         
         # seed so image and target have the same random tranform
-        seed = random.randint(0,2**32)
+        seed = random.randint(0, 2**32)
 
         img = self.imgs[idx]
         random.seed(seed)
-        img = self.transforms(img)
+        img = trans_fn2(img)
 
 
         mask = self.masks[idx]
         random.seed(seed)
-        mask = self.transforms(mask)
+        mask = trans_fn2(mask)
 
         target = self.targets[idx]
         random.seed(seed)
-        target = self.transforms(target)
+        target = trans_fn2(target)
 
         # fig = plt.figure()
         # ax1 = fig.add_subplot(1,3,1)
@@ -104,101 +114,125 @@ class UNetDataset(Dataset):
 
 
 class model:
-    def __init__(self, data_path):
-        self.data_path = data_path
+
+    def __init__(self, model):
         
-        self.network = Network()
-        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=1e-3)
-        self.loss_fn = nn.BCELoss()
-
-        # training iteration
-        self.epoch = 100
-
-    def train(self):
+        self.model = model
+        if self.model == 'U-Net':
+            self.network = UNet()
+        elif self.model == 'R2U-Net':
+            self.network = R2UNet()
+        elif self.model == 'IterNet':
+            self.network = IterNet()
+    def train(self, epoch):
         self.network.to(device)
         self.network.train()
-        training_set = UNetDataset(os.path.join(self.data_path, 'training'), trans)
-        training_loader = DataLoader(training_set, batch_size=16, shuffle=True)
-        for i in range(self.epoch):
+
+        optimizer = optim.Adam(self.network.parameters())
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.1)
+        loss_fn = nn.BCELoss()
+
+        for i in range(epoch):
             print('{} epoch {} {}'.format('=' * 10, i, '=' * 10))
             sum_loss = 0
-            
+            training_set = UNetDataset('./DRIVE/training', trans_fn2)
+            training_loader = DataLoader(training_set, batch_size=16, shuffle=True)
             for img, mask, target in tqdm(training_loader):
 
                 predict = self.network(img)
 
-                predict = predict.view(predict.size(0), -1)
-                predict = predict * mask.view(mask.size(0), -1)
-                target = target.view(target.size(0), -1)
+                if self.model == 'IterNet':
+                    mask = mask.view(mask.size(0), -1)
+                    target = target.view(target.size(0), -1)
+                    loss = 0
+                    for j in range(3):
+                        iter_predict = predict[j].view(predict[j].size(0), -1)
+                        iter_predict = iter_predict * mask.view(mask.size(0), -1)
 
-                self.optimizer.zero_grad()
-                loss = self.loss_fn(predict, target)
-                sum_loss += loss.item()
+                        loss += loss_fn(iter_predict, target)
+
+                    sum_loss += loss.item()
+                else:
+                    predict = predict.view(predict.size(0), -1)
+                    predict = predict * mask.view(mask.size(0), -1)
+                    target = target.view(target.size(0), -1)
+
+
+                    loss = loss_fn(predict, target)
+                    sum_loss += loss.item()
+
+                optimizer.zero_grad()
                 loss.backward()
-                self.optimizer.step()
+                optimizer.step()
 
-            sum_loss /= 184
+            scheduler.step()
+
+            sum_loss /= 230
             print('loss: {}'.format(sum_loss))
 
-            if i % 10 == 0:
-                torch.save(self.network.state_dict(), './model{}.pth'.format(i))
+            if i % 5 == 0:
+                torch.save(self.network.state_dict(), './{}{}.pth'.format(self.model, i))
 
-        torch.save(self.network.state_dict(), './model.pth')
-
-    # def predict(self):
-    #     self.network.load_state_dict(torch.load('./model.pth'))
-    #     img = Image.open('./DRIVE/testing/images/01_test.tif')
-    #     img = np.expand_dims(np.transpose(np.array(img, dtype=np.float32), (2, 0, 1)), axis=0)
-    #     img = torch.from_numpy(img).to(device)
-    #     predict = self.network(img)
+        torch.save(self.network.state_dict(), './{}.pth'.format(self.model))
 
     
-    def test(self, show=False):
+    def test(self, show):
         '''
         run test set
         '''
         # load saved model
         self.network.to('cpu')
-        self.network.load_state_dict(torch.load('./model.pth', map_location=torch.device('cpu')))
+        self.network.load_state_dict(torch.load('./{}.pth'.format(self.model), map_location=torch.device('cpu')))
         self.network.eval()
 
         # load test set
-        self.imgs_path = list(sorted(os.listdir(os.path.join(self.data_path, "testing/images"))))
-        self.masks_path = list(sorted(os.listdir(os.path.join(self.data_path, "testing/mask"))))
-        self.targets_path = list(sorted(os.listdir(os.path.join(self.data_path, "testing/1st_manual"))))
+        self.imgs_path = list(sorted(os.listdir('./DRIVE/testing/images')))
+        self.masks_path = list(sorted(os.listdir('./DRIVE/testing/mask')))
+        self.targets_path = list(sorted(os.listdir('./DRIVE/testing/1st_manual')))
         
-        ac_list = []
+        results = []
         with torch.no_grad():
             for img_name, mask_name, target_name in zip(self.imgs_path, self.masks_path, self.targets_path):
-                img_path = os.path.join(self.data_path, "testing/images", img_name)
+                img_path = os.path.join('./DRIVE/testing/images', img_name)
                 img = Image.open(img_path)
                 img = transforms.functional.center_crop(img, 560)
                 img = transforms.functional.to_tensor(img).unsqueeze(0)
 
-                mask_path = os.path.join(self.data_path, "testing/mask", mask_name)
+                mask_path = os.path.join('./DRIVE/testing/mask', mask_name)
                 mask = Image.open(mask_path)
                 mask = transforms.functional.center_crop(mask, 560)
-                mask = np.array(mask)
+                mask = np.array(mask).flatten() / 255
+                mask = mask.astype(np.uint8)
 
-                target_path = os.path.join(self.data_path, "testing/1st_manual", target_name)
+
+                target_path = os.path.join('./DRIVE/testing/1st_manual', target_name)
                 target = Image.open(target_path)
                 target = transforms.functional.center_crop(target, 560)
                 target = np.array(target)
+                target_ = target.flatten() / 255
+                target_ = target_.astype(np.uint8)
+                target_ = target_[mask==1]
 
                 predict = self.network(img)
                 predict = np.squeeze(predict.numpy(), axis=(0,1))
-                predict = (predict>=0.5).astype(np.uint8)
+                predict_ = predict.flatten()[mask==1]
+                predict_ = (predict_>=0.5).astype(np.uint8)
 
-                predict = predict * mask
 
-                TP = np.sum(np.logical_and(predict == 1, target == 1))
-                TN = np.sum(np.logical_and(predict == 0, target == 0))
-                # FP = np.sum(np.logical_and(predict == 1, target == 0))
-                # FN = np.sum(np.logical_and(predict == 0, target == 1))
-                AC = (TP+TN)/(560*560)
-                ac_list.append(AC)
+                TP = np.sum(np.logical_and(predict_ == 1, target_ == 1)) # true positive
+                TN = np.sum(np.logical_and(predict_ == 0, target_ == 0)) # true negative
+                FP = np.sum(np.logical_and(predict_ == 1, target_ == 0)) # false positive
+                FN = np.sum(np.logical_and(predict_ == 0, target_ == 1)) # false negative
 
-                # print(AC)
+                AC = (TP+TN)/(TP+TN+FP+FN) # accuracy
+                SE = (TP)/(TP+FN) # sensitivity
+                SP = TN/(TN+FP) # specificity
+                precision = TP/(TP+FP)
+                recall = TP/(TP+FN)
+                F1 = 2*((precision*recall)/(precision+recall))
+                fpr, tpr, _ = roc_curve(target_, predict_)
+                AUC = auc(fpr,tpr)
+                results.append((F1, SE, SP, AC, AUC))
 
                 # show predicted image
                 if show:
@@ -211,11 +245,30 @@ class model:
                     ax3.imshow(target, cmap="gray")
                     plt.show()
 
-        print('accuracy: %.4f' %(sum(ac_list)/20))
+        F1, SE, SP, AC, AUC = map(list, zip(*results))
+
+        print('accuracy: %.4f' %(sum(F1)/len(F1)))
+        print('sensitivity: %.4f' %(sum(SE)/len(SE)))
+        print('specificity: %.4f' %(sum(SP)/len(SP)))
+        print('Dice coefficient: %.4f' %(sum(AC)/len(AC)))
+        print('Jaccard similarity: %.4f' %(sum(AUC)/len(AUC)))
 
 
 if __name__ == '__main__':
 
-    m = model('./DRIVE')
-    # m.train()
-    m.test(True)
+    parser = argparse.ArgumentParser(description='U-Net')
+    # general setting
+    parser.add_argument('--model', type=str, default='U-Net', help='U-Net R2U-Net IterNet')
+    parser.add_argument('--mode', type=str, default='test', help='train test')
+    # training setting
+    parser.add_argument('--epoch', type=int, default=45, help='training epoch')
+    # testing setting
+    parser.add_argument('--show', type=bool, default=True, help='if show the predicted image')
+
+    args = parser.parse_args()
+
+    m = model(args.model)
+    if args.mode == 'train':
+        m.train(args.epoch)
+    else:
+        m.test(args.show)
--- model.py
+++ model.py
@@ -35,7 +35,7 @@ class RRC_block(nn.Module):
 
         return x+res_x
 
-class Network(nn.Module):
+class R2UNet(nn.Module):
     def __init__(self):
         super().__init__()
 
@@ -231,3 +231,372 @@ class up_conv(nn.Module):
 #         out = self.Conv1x1(d2)
 
 #         return torch.sigmoid(out)
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(256, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+        )
+
+        self.trans_conv = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(512, 1024, 3, 1, 1),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(True),
+            nn.Conv2d(1024, 1024, 3, 1, 1),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(True),
+            nn.Conv2d(1024, 1024, 3, 1, 1),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2),
+        )
+
+        self.up_conv1 = nn.Sequential(
+            nn.Conv2d(1024, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2),
+        )
+
+        self.up_conv2 = nn.Sequential(
+            nn.Conv2d(512, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),
+        )
+
+        self.up_conv3 = nn.Sequential(
+            nn.Conv2d(256, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+        )
+
+        self.final_conv = nn.Sequential(
+            nn.Conv2d(128, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Conv2d(64, 1, 1),
+        )
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+
+        x1 = self.conv1(x)
+        x2 = self.conv2(x1)
+        x3 = self.conv3(x2)
+        x4 = self.conv4(x3)
+
+        x = self.trans_conv(x4)
+
+        x = self.up_conv1(torch.cat((x, x4), dim=1))
+        x = self.up_conv2(torch.cat((x, x3), dim=1))
+        x = self.up_conv3(torch.cat((x, x2), dim=1))
+        x = self.final_conv(torch.cat((x, x1), dim=1))
+
+        x = self.sigmoid(x)
+        
+        return x
+
+class MainUNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 32, 3, 1, 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 3, 1, 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(32, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+        )
+
+        self.trans_conv = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(256, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2),
+        )
+
+        self.up_conv1 = nn.Sequential(
+            nn.Conv2d(512, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),
+        )
+
+        self.up_conv2 = nn.Sequential(
+            nn.Conv2d(256, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+        )
+
+        self.up_conv3 = nn.Sequential(
+            nn.Conv2d(128, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),
+        )
+
+        self.up_conv4 = nn.Sequential(
+            nn.Conv2d(64, 32, 3, 1, 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 3, 1, 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            # nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+        )
+
+        self.conv_1x1 = nn.Conv2d(32, 1, 1)
+    
+    def forward(self, x):
+        latent1 = self.conv1(x)
+        x2 = self.conv2(latent1)
+        x3 = self.conv3(x2)
+        x4 = self.conv4(x3)
+        x = self.trans_conv(x4)
+
+        x = self.up_conv1(torch.cat((x, x4), dim=1))
+        x = self.up_conv2(torch.cat((x, x3), dim=1))
+        x = self.up_conv3(torch.cat((x, x2), dim=1))
+        latent2 = self.up_conv4(torch.cat((x, latent1), dim=1))
+
+        x = self.conv_1x1(latent2)
+
+        return latent1, latent2, torch.sigmoid(x)
+
+class MiniUNet(nn.Module):
+    def __init__(self, iter=2):
+        super().__init__()
+
+        self.transpose = nn.Sequential(
+            nn.Conv2d(32, 32, 3, 1, 1),
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 3, 1, 1),
+            nn.ReLU(True),
+        )
+
+        self.dim_reduc = nn.ModuleList([nn.Conv2d(64+32*i, 32, 1, 1) for i in range(iter)])
+
+        self.conv1 = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(32, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+        )
+
+        self.trans_conv = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),
+        )
+
+        self.up_conv1 = nn.Sequential(
+            nn.Conv2d(256, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+        )
+
+        self.up_conv2 = nn.Sequential(
+            nn.Conv2d(128, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),
+        )
+
+        self.up_conv3 = nn.Sequential(
+            nn.Conv2d(64, 32, 3, 1, 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 3, 1, 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+
+        )
+
+        self.conv_1x1 = nn.Conv2d(32, 1, 1)
+
+    def forward(self, latent1, latent2):
+        latent3 = self.transpose(latent2)
+        latent1 = torch.cat((latent1, latent3), dim=1)
+        idx = int((latent1.size(1)-64)/32)
+        x1 = self.dim_reduc[idx](latent1)
+        x2 = self.conv1(x1)
+        x3 = self.conv2(x2)
+        x = self.trans_conv(x3)
+
+        x = self.up_conv1(torch.cat((x, x3), dim=1))
+        x = self.up_conv2(torch.cat((x, x2), dim=1))
+        latent2 = self.up_conv3(torch.cat((x, x1), dim=1))
+
+        x = self.conv_1x1(latent2)
+
+        return latent1, latent2, torch.sigmoid(x)
+
+
+        
+
+class IterNet(nn.Module):
+    def __init__(self, t=2):
+        super().__init__()
+        self.iter = t
+        
+        self.main = MainUNet()
+        self.mini = MiniUNet()
+
+    def forward(self, x):
+        out_list = []
+        latent1, latent2, out = self.main(x)
+        out_list.append(out)
+
+        for _ in range(self.iter):
+            latent1, latent2, out = self.mini(latent1, latent2)
+            out_list.append(out)
+
+        return out_list
+    
